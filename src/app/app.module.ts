4e1b5da173b9923d8122045e13f8d6b8e80091d4
Mano1405 <manoharwadgaonkar444@gmail.com>
2022-11-20 15:51:48 +0530

    Project Upload on git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ShearedModule } from './sheared/sheared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShearedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
